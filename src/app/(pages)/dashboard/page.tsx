58d732f566614303ada74e4806fc453bc62a08d3
aashish <quakie.exe@gmail.com>
2024-06-09 16:14:12 +0530

    crying

--- src/app/(pages)/dashboard/page.tsx
+++ src/app/(pages)/dashboard/page.tsx
@@ -1,4 +1,4 @@
-import { authOptions } from "../../utils/auth"
+import { authOptions } from "../../utils/auth"; 
 import Avatar from "@/components/Avatar"
 import MainSection from "@/components/MainSection"
 import { Button } from "@/components/ui/button"
