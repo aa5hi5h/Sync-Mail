01bae3f2d7adcfd05647534bff6e879aed583ea2
aashish <quakie.exe@gmail.com>
2024-06-10 01:20:44 +0530

    addedd a github button

--- src/components/TryButton.tsx
+++ src/components/TryButton.tsx
@@ -1,12 +1,13 @@
 "use client"
 import { useRouter } from "next/navigation"
 import { Button } from "./ui/button"
-
+import {Github } from "lucide-react"
 
 const TryButton = () => {
     const router = useRouter()
     return (
-        <Button onClick={() => router.push("/sign-in")}>Try free</Button>
+        <Button className="gap-x-2" onClick={() => router.push("https://github.com/aa5hi5h/Sync-Mail")}>
+            <Github size={22} /> Github </Button>
     )
 }
 
