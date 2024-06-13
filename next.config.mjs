3bc7b590e1e50dd6ae018d5488aa5af1394d24ed
quakie <156516764+aa5hi5h@users.noreply.github.com>
2024-06-13 11:00:04 +0530

    Updated config file

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,12 @@
 const nextConfig = {
     images:{
        domains: ['lh3.googleusercontent.com'],
-    }
+    },
+     env: {
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+  },
 };
 
 export default nextConfig;
